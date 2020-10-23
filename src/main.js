74b439972d7fe595b86df5c16aaa76743ab382fb
zx <1270474603@qq.com>
2020-10-23 17:51:55 +0800

    Nowplaying.vue

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
+// import './assets/styles/reset.css'
 // import 'swiper/swiper-bundle.css'
 
 Vue.config.productionTip = false
--- src/views/film/components/Nowplaying.vue
+++ src/views/film/components/Nowplaying.vue
@@ -1,46 +1,134 @@
 <template>
   <div>
-    nowplaying
+    <ul>
+      <li v-for="data in datalist" :key="data.filmId" >
+        
+          <img :src="data.poster" alt="">
+        
+        <div class="film-content">
+          <div><span class="film-title">{{data.name}}</span><span class="film-effect">{{data.item.name}}</span></div>
+          <div><span>观众评分<span class="film-grade">{{data.grade}}</span></span></div>
+          <div><span>主演:{{data.actors | actorfilter}}</span></div>
+          <div><span>{{data.nation}}&nbsp;|&nbsp;{{data.runtime}}分钟</span></div>
+        </div>
+          <div class="film-buy"> 
+            购票
+          </div>
+      </li>
+    </ul>
+    <div  class="film-occupy">
+
+    </div>
   </div>
 </template>
 
 <script>
-// import Vue from 'vue'
-// import axios from 'axios'
-export default {
-//   name:"Nowplaying",
-//   data() {
-//     return {
-//       datalist:[],
+import Vue from 'vue'
+import axios from 'axios'
 
-//     }
-//   },
-//   mounted(){
-//       this.getdata()
+Vue.filter("actorfilter",function(data){
+    var newlist = data.map(item=>item.name)
+    // console.log(newlist.join(' '))
+    return newlist.join(' ')
+})
+
+export default {
+  name:"Nowplaying",
+  data() {
+    return {
+      datalist:[],
+
+    }
+  },
+  mounted(){
+      this.getdata()
         
        
-//     },
-//   methods: {
-//     getdata () {
-//       axios({
-//               url:"https://m.maizuo.com/gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=1820379",
-//               headers:{
-//                   'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"16017302651565962255990785"}',
-//                   'X-Host': 'mall.film-ticket.film.list'
-//               }
-//           }).then(res=>{
-//               console.log(res.data)
-//               this.datalist=res.data.data.films
-//               console.log(this.datalist)
-//               // this.datalist=[...this.datalist,...res.data.data.films];
-//               // this.total = res. data.data.total
-//           })
-//      }
-// }
+    },
+  methods: {
+    getdata () {
+      axios({
+              url:"https://m.maizuo.com/gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=1820379",
+              headers:{
+                  'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"16017302651565962255990785"}',
+                  'X-Host': 'mall.film-ticket.film.list'
+              }
+          }).then(res=>{
+              console.log(res.data)
+              this.datalist=res.data.data.films
+              console.log(this.datalist)
+              // this.datalist=[...this.datalist,...res.data.data.films];
+              // this.total = res. data.data.total
+          })
+     }
+}
 }
 
 </script>
 
 <style lang="scss" scoped>
-
+  ul{
+        li{
+            display: flex;
+            padding: 1rem;
+            overflow: hidden;
+            border-bottom: 1px solid #f3f3f3;
+            img{
+                float: left;
+                width: 18.5vw;
+                padding-right:0.8rem;
+                // flex: 0.5;
+            }
+            span{
+                font-size: 0.81rem;
+                color: #191a1b;
+                font-weight: 200;
+            }
+            .film-title{
+                font-size: 1rem;
+                font-weight:500;
+            }
+            .film-effect{
+                display: inline-block;
+                vertical-align: middle;
+                font-size: 0.56rem;
+                color: white;
+                height: 0.87rem;
+                line-height: 0.87rem;
+                font-weight: 400;
+                background-color: #d2d6dc;
+                padding: 0 0.18rem;
+                margin: 0 0.3rem;
+                border-radius: 2px;
+            }
+            .film-grade{
+              font-size: 0.92rem;
+              color: #ffb232;
+              font-weight: 400;
+            }
+            .film-content{
+              float: left;
+              // width: ;
+              flex: 2;
+            }
+            .film-buy{
+              float: right;
+              height: 1.5rem;
+              line-height: 1.5rem;
+              width: 3rem;
+              font-size: 0.81rem;
+              text-align: center;
+              border: 1px solid #ff5f16;
+              color: #ff5f16;
+              border-radius: 0.2rem;
+              flex: 0.5;
+              align-self:center;
+              cursor: pointer;
+            }
+        }
+    }
+    .film-occupy{
+      height: 3.75rem;
+      background: #f4f4f4;
+    }
 </style>
